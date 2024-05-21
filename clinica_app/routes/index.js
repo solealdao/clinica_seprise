c1d637faa7d88716547244ace5a72f5f559cab3c
solealdao <soledad_aldao@hotmail.com>
2024-05-20 21:18:27 -0300

    se agregó hojas de estilo a los archivos html existentes

--- clinica_app/routes/index.js
+++ clinica_app/routes/index.js
@@ -7,4 +7,14 @@ router.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+/* GET admin home page. */
+router.get('/admin-home', function (req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/admin-home.html'));
+});
+
+/* GET doctor home page. */
+router.get('/doctor-home', function (req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/doctor-home.html'));
+});
+
 module.exports = router;
