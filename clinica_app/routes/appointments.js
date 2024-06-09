ac4f6b8f95bdea2896944b94e2220f1d2f2774c5
Estela <estela.vm@hotmail.com.ar>
2024-06-09 20:08:47 -0300

    Incorporada Gestión de pagos en Consultar turno. Falta terminar funcionalidad

--- clinica_app/routes/appointments.js
+++ clinica_app/routes/appointments.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 var appointmentsController = require('../controllers/appointmentsController');
 
+function loadAppointments() {
+    const data = fs.readFileSync(appointmentsFilePath, 'utf-8');
+    return JSON.parse(data);
+}
+
+function saveAppointments(appointments) {
+    const data = JSON.stringify(appointments, null, 2);
+    fs.writeFileSync(appointmentsFilePath, data, 'utf-8');
+}
+
 router.get('/appointment-management', function (req, res, next) {
 	res.render('appointment-management');
 });
 
 router.get('/appointment-new', appointmentsController.renderNewAppointment);
 router.post('/appointment-new', appointmentsController.submitNewAppointment);
+
+router.get('/appointment-pay', function(req, res, next) {
+    res.render('appointment-pay'); 
+});
 
 router.get('/appointment-receipt', appointmentsController.renderShiftReceipt);
 
