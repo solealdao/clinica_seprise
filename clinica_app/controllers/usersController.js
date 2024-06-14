fa9601d4248620c60990d86691bf2a4953286f2f
solealdao <soledad_aldao@hotmail.com>
2024-06-14 01:19:52 -0300

    flujo de gestion de usuarios completo, unificacion de estilos

--- clinica_app/controllers/usersController.js
+++ clinica_app/controllers/usersController.js
@@ -3,105 +3,157 @@ const fs = require('fs');
 const usersFilePath = path.join(__dirname, '../data/login.json');
 
 function loadUsers() {
-    const data = fs.readFileSync(usersFilePath, 'utf-8');
-    return JSON.parse(data).usuarios;
+	const data = fs.readFileSync(usersFilePath, 'utf-8');
+	return JSON.parse(data).usuarios;
 }
 
 function saveUsers(users) {
-    const data = JSON.stringify({ usuarios: users }, null, 2);
-    fs.writeFileSync(usersFilePath, data, 'utf-8');
+	const data = JSON.stringify({ usuarios: users }, null, 2);
+	fs.writeFileSync(usersFilePath, data, 'utf-8');
 }
 
 let controllerUsers = {
-    renderHome: (req, res) => {
-        res.render('index');
-    },
-    login: (req, res) => {
-        const { email, password } = req.body;
-        const usuarios = loadUsers(); // Cargar usuarios desde el archivo
-        const userlogged = usuarios.find(
-            (user) => user.email === email && user.password === password
-        );
+	renderHome: (req, res) => {
+		res.render('index');
+	},
+	login: (req, res) => {
+		const { email, password } = req.body;
+		const usuarios = loadUsers();
+		const userlogged = usuarios.find(
+			(user) => user.email === email && user.password === password
+		);
 
-        if (!userlogged) {
-            return res.status(401).send('Credenciales inválidas');
-        }
+		if (!userlogged) {
+			return res.status(401).send('Credenciales inválidas');
+		}
 
-        if (userlogged.permisos === 'admin') {
-            res.redirect('/home-admin');
-        } else if (userlogged.permisos === 'medico') {
-            res.redirect('/home-doctor');
-        } else if (userlogged.permisos === 'tecnico') {
-            res.redirect('/home-technical');
-        } else {
-            res.status(403).send('Acceso no autorizado');
-        }
-    },
-    renderUsersManagement: (req, res) => {
-        res.render('user-management');
-    },
-    renderUsersManagement2: (req, res) => {
-        const users = loadUsers();
-        res.render('user-management', { users });
-    },
-    renderUserNew: (req, res) => {
-        res.render('user-new');
-    },
-    renderUserProfile: (req, res) => {
-        res.render('user-profile');
-    },
-    renderPatientProfile : (req, res) => {
-        res.render('patient-profile');
-    },
-    createUser: (req, res) => {
-        try {
-            const { email, password, permisos } = req.body;
-            const usuarios = loadUsers();
-            usuarios.push({ email, password, permisos });
-            saveUsers(usuarios);
-            res.redirect('/users/user-management');
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Lo sentimos, ha ocurrido un error.');
-        }
-    },
-    renderUserDelete: (req, res) => {
-        const users = loadUsers();
-        res.render('user-delete', { users });
-    },
-    deleteUser: (req, res) => {
-        try {
-            const { email } = req.body;
-            let usuarios = loadUsers();
-            usuarios = usuarios.filter(user => user.email !== email);
-            saveUsers(usuarios);
-            res.redirect('/users/user-management');
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Lo sentimos, ha ocurrido un error.');
-        }
-    },
-	renderUserUpdate: (req, res) => {
-        res.render('user-update');
-    },
+		if (userlogged.permisos === 'admin') {
+			res.redirect('/home-admin');
+		} else if (userlogged.permisos === 'medico') {
+			res.redirect('/home-doctor');
+		} else if (userlogged.permisos === 'tecnico') {
+			res.redirect('/home-technical');
+		} else {
+			res.status(403).send('Acceso no autorizado');
+		}
+	},
+	renderUsersManagement: (req, res) => {
+		res.render('user-management');
+	},
+	renderUserNew: (req, res) => {
+		res.render('user-new');
+	},
+	renderUserProfile: (req, res) => {
+		res.render('user-profile');
+	},
+	renderPatientProfile: (req, res) => {
+		res.render('patient-profile');
+	},
+	createUser: (req, res) => {
+		try {
+			const { email, password, permisos } = req.body;
+			const usuarios = loadUsers();
+			usuarios.push({ email, password, permisos });
+			saveUsers(usuarios);
+			res.redirect(
+				'/users/user-new?success=El+usuario+ha+sido+creado+exitosamente.'
+			);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Lo sentimos, ha ocurrido un error.');
+		}
+	},
+	renderUserSearch: (req, res) => {
+		res.render('user-search');
+	},
+	getUserByMail: (req, res) => {
+		const email = req.body.email;
 
-    updateUser: (req, res) => {
-        try {
-            const { email, password, permisos } = req.body;
-            let usuarios = loadUsers();
-            const index = usuarios.findIndex(user => user.email === email);
-            if (index !== -1) {
-                usuarios[index] = { email, password, permisos };
-                saveUsers(usuarios);
-                res.redirect('/users/user-management');
-            } else {
-                res.status(404).send('Usuario no encontrado');
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Lo sentimos, ha ocurrido un error.');
-        }
-    }
+		try {
+			const users = loadUsers();
+			const usuarioFiltrado = users.filter((pac) => pac.email === email);
+
+			if (usuarioFiltrado.length === 0) {
+				return res.render('user-search', {
+					usuarioFiltrado: [],
+					noResults: true,
+				});
+			}
+			res.render('user-search', { usuarioFiltrado, noResults: false });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: 'Error al buscar usuarios',
+				error: error.message,
+			});
+		}
+	},
+	renderUpdateUser: (req, res) => {
+		const idUser = parseInt(req.params.idUser, 10);
+
+		try {
+			const totalUsers = loadUsers();
+			const user = totalUsers.find((user) => user.idUser === idUser);
+			if (!user) {
+				return res.status(404).json({ message: 'Usuario no encontrado' });
+			}
+			res.render('user-update', { user });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: 'Error al cargar los datos del usuario',
+				error: error.message,
+			});
+		}
+	},
+	saveEditedUser: (req, res) => {
+		const { idUser, email, password, permisos } = req.body;
+
+		try {
+			let users = loadUsers();
+			const index = users.findIndex(
+				(user) => user.idUser === parseInt(idUser, 10)
+			);
+
+			if (index !== -1) {
+				users[index] = {
+					...users[index],
+					email,
+					password,
+					permisos,
+				};
+				saveUsers(users);
+				res.status(200).json({ message: 'Usuario modificado con éxito' });
+			} else {
+				res.status(404).json({ message: 'Usuario no encontrado' });
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: 'Error al editar el usuario',
+				error: error.message,
+			});
+		}
+	},
+	renderDeleteUser: (req, res) => {
+		const users = loadUsers();
+		res.render('patient-delete', { users });
+	},
+	deleteUser: (req, res) => {
+		const idUser = parseInt(req.body.idUser, 10);
+		try {
+			let users = loadUsers();
+			users = users.filter((user) => user.idUser !== idUser);
+			saveUsers(users);
+			res.status(200).json({ message: 'Usuario eliminado con éxito' });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: 'Error al eliminar el usuario',
+				error: error.message,
+			});
+		}
+	},
 };
 
 module.exports = controllerUsers;
--- clinica_app/routes/users.js
+++ clinica_app/routes/users.js
@@ -3,63 +3,35 @@ var router = express.Router();
 var usersController = require('../controllers/usersController');
 const fs = require('fs');
 const path = require('path');
-
 const usersFilePath = path.join(__dirname, '../data/login.json');
-
-function loadUsers() {
-    const data = fs.readFileSync(usersFilePath, 'utf-8');
-    return JSON.parse(data).usuarios;
-}
-
-function saveUsers(users) {
-    const data = JSON.stringify({ usuarios: users }, null, 2);
-    fs.writeFileSync(usersFilePath, data, 'utf-8');
-}
 
 // Verificar permisos de lectura y escritura
 fs.access(usersFilePath, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-    if (err) {
-        console.error(`${usersFilePath} no tiene los permisos necesarios`);
-    } else {
-        console.log(`${usersFilePath} tiene permisos de lectura y escritura`);
-    }
+	if (err) {
+		console.error(`${usersFilePath} no tiene los permisos necesarios`);
+	} else {
+		console.log(`${usersFilePath} tiene permisos de lectura y escritura`);
+	}
 });
 
-// Ruta para renderizar la gestión de usuarios
 router.get('/user-management', usersController.renderUsersManagement);
 
-// Ruta para renderizar la creación de usuarios
-router.get('/create-user', (req, res) => {
-    res.render('create-user');
-});
-
-// Ruta para manejar la creación de usuarios
+router.get('/user-new', usersController.renderUserNew);
 router.post('/create', usersController.createUser);
 
-// Ruta para renderizar la gestión de usuarios con la nueva lista de usuarios
-router.get('/user-management2', usersController.renderUsersManagement2);
+router.get('/user-search', usersController.renderUserSearch);
+router.post('/user-search', usersController.getUserByMail);
 
-// Ruta para renderizar la vista de nuevo usuario
-router.get('/user-new', usersController.renderUserNew);
+router.get('/user-update/:idUser', usersController.renderUpdateUser);
+router.post('/user-save', usersController.saveEditedUser);
 
-// Ruta para renderizar la eliminación de usuarios
-router.get('/user-delete', usersController.renderUserDelete);
+router.get('/user-delete', usersController.renderDeleteUser);
+router.post('/user-delete', usersController.deleteUser);
 
-// Ruta para manejar la eliminación de usuarios
-router.post('/delete', usersController.deleteUser);
-
-// Ruta para renderizar la vista de modificar usuario
-router.get('/user-update', usersController.renderUserUpdate);
-
-// Ruta para manejar la actualización de usuarios
-router.post('/update', usersController.updateUser);
-
-// Ruta para renderizar perfil usuario 
+// Ruta para renderizar perfil usuario
 router.get('/user-profile', usersController.renderUserProfile);
 
-// Ruta para renderizar perfil paciente 
+// Ruta para renderizar perfil paciente
 router.get('/patient-profile', usersController.renderPatientProfile);
-
-
 
 module.exports = router;
